6f036d81a689f108d12af555f75984c3f37fcee3
Mohmed Hira <mohmed.hira@slalom.com>
2025-07-09 21:06:52 +0000

    test: add coverage reporters and thresholds to Jest configuration

--- packages/backend/jest.config.js
+++ packages/backend/jest.config.js
@@ -5,4 +5,13 @@ module.exports = {
   coverageDirectory: 'coverage',
   coveragePathIgnorePatterns: ['/node_modules/'],
   testMatch: ['**/__tests__/**/*.js', '**/?(*.)+(spec|test).js'],
+  coverageReporters: ['json', 'lcov', 'text', 'clover', 'json-summary'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  }
 };
